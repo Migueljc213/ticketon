841f0f710a5e967cc51971acd590eb5d0e1b885a
Migueljc213 <migueljcbrbm@gmail.com>
2026-07-12 09:25:42 -0300

    feat: rota de status com ENVIRONMENT

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 
+
+
+
 @Controller()
 export default class AppController {
+ constructor(
+
+ ){}
+
   @Get()
   @HttpCode(HttpStatus.OK)
   root() {
-    return { name: 'Ticketon API', status: 'ok' };
+    return { name: 'Ticketon API' + process.env.ENVIRONMENT, status: 'ok' };
   }
 
   @Get('health')
